3eae2da86114b188f9f217317ede3e7f14b0efc4
Igor Kryvopustov <ihorkr@tahometer.com>
2020-03-06 10:34:40 +0200

    Test for check auto open

--- test/CheckoutServiceTest.cs
+++ test/CheckoutServiceTest.cs
@@ -29,5 +29,19 @@ namespace SilpoTest
 
             Assert.Equal(10, check.GetTotalCost());
         }
+
+        [Fact]
+        public void addProduct__whenCheckIsClosed__opensNewCheck()
+        {
+            CheckoutService checkoutService = new CheckoutService();
+
+            checkoutService.AddProduct(new Product(7, "Milk"));
+            Check milkCheck = checkoutService.CloseCheck();
+            Assert.Equal(7, milkCheck.GetTotalCost());
+
+            checkoutService.AddProduct(new Product(3, "Bread"));
+            Check breadCheck = checkoutService.CloseCheck();
+            Assert.Equal(3, breadCheck.GetTotalCost());
+        }
     }
 }
